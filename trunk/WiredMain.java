90362e104e14810f5c44db2caf4429cfb52d2e7b
Alexei.Svitkine <Alexei.Svitkine@21437e44-7bf5-11de-a624-ab7c8f4ec817>
2009-07-30 21:42:55 +0000

    WiredMain.java, which uses WiredClient.java through SSL to provide a very basic wired client / debugger

---
+++ trunk/WiredMain.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.security.*;
+import java.security.cert.*;
+import javax.net.ssl.*;
+
+public class WiredMain {
+	public static void main(String[] args) throws Exception {
+		InputStream in1 = new FileInputStream(new File("ssl_certs"));
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(in1, "changeit".toCharArray());
+		in1.close();
+
+		SSLContext context = SSLContext.getInstance("TLS");
+		String algorithm = TrustManagerFactory.getDefaultAlgorithm();
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance(algorithm);
+		tmf.init(ks);
+		context.init(null, new TrustManager[] {tmf.getTrustManagers()[0]}, null);
+		SSLSocketFactory factory = context.getSocketFactory();
+	
+		/*
+		System.out.print("Enter host to connect to : ");
+		String host = console.readLine();
+		System.out.print("Enter port to connect on : ");
+		int port = Integer.parseInt(console.readLine());
+		*/
+
+		String host = "localhost";
+		int port = 2000;
+	
+		SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+		socket.setEnabledProtocols(new String[] {"TLSv1"});
+
+		WiredClient client = new WiredClient(socket.getInputStream(), socket.getOutputStream());
+		
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+		/*
+		System.out.print("Enter nick to use : ");
+		String nick = console.readLine();
+		System.out.print("Enter username : ");
+		String user = console.readLine();
+		System.out.print("Enter password : ");
+		String pass = console.readLine();
+		int result = client.login(nick, user, (pass.length() > 0 ? pass : null));
+		*/
+
+		int result = client.login("Wired User", "guest", null);
+		if (result == 0) {
+			System.out.println("Connected.");
+			client.requestUserList(1);
+			String in = console.readLine();
+			while (in != null) {
+				client.sendChatMessage(1, in);
+				in = console.readLine();
+			}
+		}
+	}
+}
