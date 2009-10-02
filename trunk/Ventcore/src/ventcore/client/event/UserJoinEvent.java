5d563838d452bfaa00dd92ecfc5f9889159eb1ea
Alexei.Svitkine <Alexei.Svitkine@21437e44-7bf5-11de-a624-ab7c8f4ec817>
2009-10-02 23:36:47 +0000

    chatids on user events

--- trunk/Ventcore/src/ventcore/client/event/UserJoinEvent.java
+++ trunk/Ventcore/src/ventcore/client/event/UserJoinEvent.java
@@ -3,12 +3,19 @@ package ventcore.client.event;
 import ventcore.client.User;
 
 public class UserJoinEvent extends RemoteEvent {
+	private int chatId;
 	private User user;
+
+	public int getChatId() {
+		return chatId;
+	}
+	public void setChatId(int chatId) {
+		this.chatId = chatId;
+	}
 
 	public User getUser() {
 		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
 	}
