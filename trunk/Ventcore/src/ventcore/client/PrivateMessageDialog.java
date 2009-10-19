e51bee7624fd9c594a99293363d9cee549c210d2
Alexei.Svitkine <Alexei.Svitkine@21437e44-7bf5-11de-a624-ab7c8f4ec817>
2009-10-19 04:17:01 +0000

    double click to send private message

---
+++ trunk/Ventcore/src/ventcore/client/PrivateMessageDialog.java
@@ -0,0 +1,39 @@
+package ventcore.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class PrivateMessageDialog extends DialogBox {
+	private TextArea message;
+
+	public PrivateMessageDialog(final int toUserId) {
+		setText("Private Message");
+		setAnimationEnabled(true);
+		VerticalPanel p = new VerticalPanel();
+		p.add(message = new TextArea());
+		HorizontalPanel panel = new HorizontalPanel();
+		Button sendButton = new Button("Send");
+		Button cancelButton = new Button("Cancel");
+		panel.add(cancelButton);
+		cancelButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				hide();
+			}
+		});
+		panel.add(sendButton);
+		sendButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				Ventcore.sendPrivateMessage(toUserId, message.getText());
+				hide();
+			}
+		});
+		p.add(panel);
+		p.setCellHorizontalAlignment(panel, VerticalPanel.ALIGN_RIGHT);
+		setWidget(p);
+	}
+}
