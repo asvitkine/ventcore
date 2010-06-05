b19a4aa90f2be4c3985b7368a43e25fe152ea1fa
Alexei.Svitkine <Alexei.Svitkine@21437e44-7bf5-11de-a624-ab7c8f4ec817>
2010-06-05 18:22:07 +0000

    use cookies to store login info

--- Ventcore/src/ventcore/client/LoginDialog.java
+++ Ventcore/src/ventcore/client/LoginDialog.java
@@ -2,6 +2,7 @@ package ventcore.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -11,6 +12,9 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class LoginDialog extends DialogBox {
+	private static final String NICK_COOKIE = "VentcoreNick";
+	private static final String USERNAME_COOKIE = "VentcoreUsername";
+	
 	private TextBox nick;
 	private TextBox username;
 	private TextBox password;
@@ -19,14 +23,17 @@ public class LoginDialog extends DialogBox {
 		setText("Ventcore Login");
 		setAnimationEnabled(true);
 		VerticalPanel p = new VerticalPanel();
-		addField(p, "Nick:", nick = createTextBox("Ventcore User"));
-		addField(p, "Login:", username = createTextBox("guest"));
-		addField(p, "Password:", password = createTextBox(""));
+		addField(p, "Nick:", nick = createTextBox(getDefaultNick()));
+		addField(p, "Login:", username = createTextBox(getDefaultUsername()));
+		addField(p, "Password:", password = createTextBox(getDefaultPassword()));
 		Button loginButton = new Button("Login");
 		p.add(loginButton);
 		p.setCellHorizontalAlignment(loginButton, VerticalPanel.ALIGN_RIGHT);
 		loginButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
+				LoginInfo info = getLoginInfo();
+				Cookies.setCookie(NICK_COOKIE, info.getNick());
+				Cookies.setCookie(USERNAME_COOKIE, info.getUsername());
 				// FIXME: handle failures
 				Ventcore.login(getLoginInfo());
 				LoginDialog.this.hide();
@@ -35,6 +42,20 @@ public class LoginDialog extends DialogBox {
 		setWidget(p);
 	}
 
+	private String getDefaultNick() {
+		String nick = Cookies.getCookie(NICK_COOKIE);
+		return (nick != null ? nick : "Ventcore User");
+	}
+
+	private String getDefaultUsername() {
+		String username = Cookies.getCookie(USERNAME_COOKIE);
+		return (username != null ? username : "guest");
+	}
+
+	private String getDefaultPassword() {
+		return "";
+	}
+	
 	private TextBox createTextBox(String text) {
 		TextBox box = new TextBox();
 		box.setText(text);
@@ -54,8 +75,9 @@ public class LoginDialog extends DialogBox {
 		panel.setWidth("100%");
 		panel.setHeight("24px");
 		vp.add(panel);
-	}	
-	public LoginInfo getLoginInfo() {
+	}
+
+	private LoginInfo getLoginInfo() {
 		LoginInfo info = new LoginInfo();
 		info.setNick(nick.getText());
 		info.setUsername(username.getText());
